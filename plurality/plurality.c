45210a3c1a0f042e42d94c4e022cc6425c73f40b
Petros Nastas <petrospaokara47@hotmail.com>
2022-05-25 21:04:19 +0300

    Initial

---
+++ plurality/plurality.c
@@ -0,0 +1,148 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+void merge(int arr[], int s, int m, int e);
+void merge_sort(int arr[], int start, int end);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    for (int i = 0; i < candidate_count; ++i)
+    {
+        if(strcmp(name, candidates[i].name) == 0) {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int tempArr[candidate_count];
+
+    for (int i = 0; i < candidate_count; ++i)
+    {
+        c
+    }
+    return;
+}
+
+void merge(int arr[], int s, int m, int e)
+{
+    int temp[e - s];
+
+    int posL = s;
+    int posR = m+1;
+    int temp_i = 0;
+    while(posL <= m && posR <= e)
+    {
+        if(arr[posL] <= arr[posR])
+        {
+            temp[temp_i] = arr[posL];
+            ++posL;
+        }
+        else
+        {
+            temp[temp_i] = arr[posR];
+            ++posR;
+        }
+        ++temp_i;
+    }
+
+    while(posL <= m) {
+        temp[temp_i] = arr[posL];
+        ++posL;
+        ++temp_i;
+    }
+
+    while(posR <= e) {
+        temp[temp_i] = arr[posR];
+        ++posR;
+        ++temp_i;
+    }
+
+    for(int i = 0; i < temp_i; ++i)
+    {
+        arr[s + i] = temp[i];
+    }
+}
+
+void merge_sort(int arr[], int start, int end)
+{
+    if(end > start)
+    {
+        int m = start + (end - start) / 2;
+        merge_sort(arr, start, m);
+        merge_sort(arr, m+1, end);
+        merge(arr, start, m, end);
+
+    }
+    else
+    {
+        return;
+    }
+
+}
